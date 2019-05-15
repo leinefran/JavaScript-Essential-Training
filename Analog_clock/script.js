93b0a02978cc97c4f148218ed485b057476a9727
Leine Francisca Valente <cozymachete@leines-mbp.attlocal.net>
2019-05-14 17:01:19 -0700

    Added CSS and JavaScript code to move clock hands.

--- Analog_clock/script.js
+++ Analog_clock/script.js
@@ -1,3 +1,14 @@
+#!/usr/bin/node
+// a script that moves the hour, minute and second hand of a clock
+
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+let hrPosition = 20;
+let minPosition = 130;
+let secPosition = 267;
+
+HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+MINUTEHANDHAND.style.transform = "rotate(" + hrPosition + "deg)";
+SECONDHANDHAND.style.transform = "rotate(" + hrPosition + "deg)";
