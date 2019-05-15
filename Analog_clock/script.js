50857a2f6395da79c0180dc77d88233dced1b2f9
Leine Francisca Valente <cozymachete@leines-mbp.attlocal.net>
2019-05-15 13:35:30 -0700

    Added: new value to variables hrPosition, minPosition and secPosition for it to display the correct current time

--- Analog_clock/script.js
+++ Analog_clock/script.js
@@ -1,13 +1,6 @@
-#!/usr/bin/node
-// a script that moves the hour, minute and second hand of a clock
-
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
-
-let hrPosition = 20;
-let minPosition = 130;
-let secPosition = 267;
 
 var date = new Date();
 console.log(date);
@@ -16,6 +9,10 @@ let hr = date.getHours();
 let min = date.getMinutes();
 let sec = date.getSeconds();
 
+let hrPosition = hr*360/12;
+let minPosition = (min*360/60) + (sec(360/60)/60);
+let secPosition = sec*360/60;
+
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-MINUTEHANDHAND.style.transform = "rotate(" + minPosition + "deg)";
-SECONDHANDHAND.style.transform = "rotate(" + secPosition + "deg)";
+MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
