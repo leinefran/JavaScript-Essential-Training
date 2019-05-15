a8b5ab619555142b4889471d5d3060652635e4ea
Leine Francisca Valente <cozymachete@leines-mbp.attlocal.net>
2019-05-15 16:10:33 -0700

    Wrapped: the code into a Function. Added: comments. Created: interval object to store the setInterval method which calls the function runTheClock every 1k seconds.

--- Analog_clock/script.js
+++ Analog_clock/script.js
@@ -2,18 +2,26 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-var date = new Date();
-console.log(date);
+function runTheClock(){
 
-let hr = date.getHours();
-let min = date.getMinutes();
-let sec = date.getSeconds();
-console.log(hr, min, sec);
+    var date = new Date();
+    console.log(date);
 
-let hrPosition = (hr*360/12) + (min*(360/60)/12);
-let minPosition = (min*360/60) + (sec*(360/60)/60);
-let secPosition = sec*360/60;
+    /* retrieves the current hr, min and sec from the date object */
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    console.log(hr, min, sec);
 
-HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+    /* fancy mat to ensure the hr, min, sec hands are moved accordingly to the current time */
+    let hrPosition = (hr*360/12) + (min*(360/60)/12);
+    let minPosition = (min*360/60) + (sec*(360/60)/60);
+    let secPosition = sec*360/60;
+
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
+}
+
+// sets the clock to run every 1k seconds.
+var interval = setInterval(runTheClock, 1000);
