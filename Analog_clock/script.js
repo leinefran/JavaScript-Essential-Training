24cf1cc9cb510921230564511ec54b2e57db4b84
Leine Francisca Valente <cozymachete@leines-mbp.attlocal.net>
2019-05-15 15:35:09 -0700

    Fixed: math to calculate the degree for min and hour position.

--- Analog_clock/script.js
+++ Analog_clock/script.js
@@ -8,9 +8,10 @@ console.log(date);
 let hr = date.getHours();
 let min = date.getMinutes();
 let sec = date.getSeconds();
+console.log(hr, min, sec);
 
-let hrPosition = hr*360/12;
-let minPosition = (min*360/60) + (sec(360/60)/60);
+let hrPosition = (hr*360/12) + (min*(360/60)/12);
+let minPosition = (min*360/60) + (sec*(360/60)/60);
 let secPosition = sec*360/60;
 
 HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
